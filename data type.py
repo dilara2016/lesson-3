efc9b8df6506282deee4fc5da2abb91e0ac8d518
dilara2016 <m.123.hunain@gmail.com>
2024-10-05 11:42:39 +0200

    print commit

---
+++ data type.py
@@ -0,0 +1,18 @@
+name="penguin"
+age = 15
+is_student = True
+weight = 38.5
+#printing diffrent varibles and there data type
+print("Name :",name)
+print("data type of Name is",type(name))
+print("Age:",age)
+print("data type of Age is",type(age))
+print("Is_student :",)
+print("data type of Is_student is",type(is_student))
+print("Weigt:",weight)
+print("data type of Weight is",type(weight))
+#type casting to convert the datatype of varibles
+print("/n  After type casting....")
+age = str(age)
+print("Age:",age)
+print("data type of Age is",type(age))
